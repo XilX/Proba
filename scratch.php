55ec781a46bf6a00acb6575f4649dad2df362146
Nikita <yushcov@bk.ru>
2019-02-07 18:23:36 +0300

    123

--- scratch.php
+++ scratch.php
@@ -1,37 +1,54 @@
 <pre><?php
-    //array_product
+
+//array_product
+
     $a = array(2, 4, 6, 8);
+
     echo array_product($a);
-    $result = 1;
+     $result = 1;
     foreach($a as $val) {
-    $result *= $val;}
+     $result *= $val;
+    }
     echo $result;
     echo "<br>";
-    //array_sum
+
+//array_sum
+
     echo array_sum($a);
-    $result = 0;
+     $result = 0;
     foreach($a as $val) {
-    $result += $val;}
+     $result += $val;
+    }
     echo $result;
     echo "<br>";
-    //array_search
+
+//array_search
+
     echo array_search(8, $a);
-    foreach($a as $key=>$val) {
-    if($val == 8) {
-    echo $key;}}
+     foreach($a as $key=>$val) {
+     if($val == 8) {
+    echo $key;
+    }
+    }
     echo "<br>";
-    //in_array
+
+//in_array
+
     if (in_array(8, $a)) {
-    echo "Нашел 8";}
+        echo "Нашел 8";
+    }
     foreach($a as $val) {
-    ($val == 8);}
+        ($val == 8);
+   }
     echo "<br>";
-    //array_reserve
+
+//array_reserve
+
     echo print_r(array_reverse($a));
-    $size = count($a);
-    $array = array();
+     $size = count($a);
+     $array = array();
     foreach($a as $val) {
-    $size--;
-    $array[$size] = $val;}
+     $size--;
+     $array[$size] = $val;}
     print_r($array);
 ?></pre>
