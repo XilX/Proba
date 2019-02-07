9bf485f8ab5921a78ba7ad9da402842edadb9666
Nikita <yushcov@bk.ru>
2019-02-07 17:59:43 +0300

    123

--- scratch.php
+++ scratch.php
@@ -1,6 +1,6 @@
 <pre><?php
+    //array_product
     $a = array(2, 4, 6, 8);
-
     echo array_product($a);
     $result = 1;
     foreach($a as $val) {
@@ -18,15 +18,13 @@
     echo array_search(8, $a);
     foreach($a as $key=>$val) {
     if($val == 8) {
-    echo $key;}
-    }
+    echo $key;}}
     echo "<br>";
     //in_array
     if (in_array(8, $a)) {
     echo "Нашел 8";}
     foreach($a as $val) {
-    ($val == 8);
-    }
+    ($val == 8);}
     echo "<br>";
     //array_reserve
     echo print_r(array_reverse($a));
