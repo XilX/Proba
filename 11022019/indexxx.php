2d48368b1c8052fdde6733f009083a6845633862
Nikita <yushcov@bk.ru>
2019-02-12 18:24:05 +0300

    /8

--- 11022019/indexxx.php
+++ 11022019/indexxx.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <title>Document</title>
 </head>
 <body>
-
 <?php
         $text1 = explode(" ",$_POST["text1"]);
         function sum ($a,$b)
@@ -25,13 +23,11 @@
         {
             return pow($a,$b);
         }
-        if (!empty($_POST["text3"]))
+        if (!empty($_POST["text2"]))
         {
             $res3 = call_user_func($text2[0], $text2[1],$text2[2]);
-        }
-     
+        }   
 ?>
-
             <form action="#" method="post">
                     <input type="text" name="text1" placeholder="<?php for ($i=0; $i < count($text1); $i++) { echo $text1[$i]." "; }?>"><br>
                    <input type="submit" value="Узнать"><br>
