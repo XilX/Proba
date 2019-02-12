f7f6338a28adb36ccd1a337b7cc16953c954039e
Nikita <yushcov@bk.ru>
2019-02-12 20:15:17 +0300

    12312

--- 12022019/file1.php
+++ 12022019/file1.php
@@ -1,12 +1,8 @@
 <?php
 
-
-
-
-
-
-
-
-
-
+// echo preg_match_all('/\d{3}/', $_POST["textq"], $match2);
+// echo preg_match_all('/x{1,3}/', $_POST["textq"], $match);
+echo preg_match_all('/\/\*.*?\*\//', $_POST["textq"], $match3);
+print_r($match3);
+// print_r($match);
 ?>
