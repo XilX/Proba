a72c32529a6599e4f645c27aff66f8d8014abdd2
Nikita <yushcov@bk.ru>
2019-02-19 18:02:19 +0300

    homework

--- vote.php
+++ vote.php
@@ -1,13 +1,13 @@
 <?php
+
     echo "Столько символов в слове: ";
     echo mb_strlen('Привет', 'UTF-8'); // 6 символов
 
     echo "<br>";
-    
+
     echo "Столько слов в тексте: ";
     echo mbStrWordCount('Привет, как дела? Чем занят?'); // 5 слов
 
-    
     function mbStrWordCount($string = null) {
         return count(mb_split('[\s_"]', $string));
     }
