d6203abbbcc10c8379b948f8505cd3e50df1524a
Nikita <yushcov@bk.ru>
2019-02-21 17:37:25 +0300

    987

--- homework.php
+++ homework.php
@@ -1,10 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <form action="" method="post">
     <input type="text" name="URL" value="<?=$_POST['URL']?>">
-
            <input type="submit" value="Отправить">
 </form>
-
-
+</body>
+</html>
 <?php
 if (isset($_POST['URL']) and $_POST['URL'] != '') {
     $content = file_get_contents($_POST['URL']);
@@ -15,11 +23,8 @@ if (isset($_POST['URL']) and $_POST['URL'] != '') {
     $content = preg_replace('/\<.*\>/siU','',$content);
     $content = htmlspecialchars($content);
 }
-
-?>
-<?php
-    for($i=1;$i<10;$i++) {
-        echo "Слов с ".$i." буквой: ";
+    for($i=1;$i<40;$i++) {
+        echo $i." букв: ";
         $count = preg_match_all('/\b\w{' .$i. '}\b/u',$content,$match,PREG_PATTERN_ORDER);
         echo $count.'<br>';
     }
