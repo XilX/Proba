45e1ac49b68f9d15a1313022afb3133b261e9925
Nikita <yushcov@bk.ru>
2019-03-14 18:21:33 +0300

    7

---
+++ phpandsql/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    print_r($_POST);
+
+    $link = mysqli_connect("localhost", "root", "", "shop");
+
+    if (mysqli_connect_errno()) {
+        echo "Не удалось подключиться к MySQL: (" .
+            mysqli_connect_errno() . ") " . mysqli_connect_error();
+    }
+
+    $res = mysqli_query($link, "SELECT * FROM customers");
+
+    echo "<table border='1'>";
+    while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+        echo "<tr>";
+        foreach ($row as $value) {
+            echo "<td>$value</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    mysqli_free_result($res);
+
+    mysqli_close($link);
+    ?><br><br>
+    <fieldset>
+        <form action="#" method="post">
+            <input name="nik" type="text" placeholder="Введите свой ник"><br><br>
+            <textarea name="msg" id="" cols="30" rows="10" placeholder="Введите сообщение"></textarea><br><br>
+            <input type="submit"> <input type="reset" value="Сбросить"><br>
+        </form>
+    </fieldset>
+
+    <?php
+    ?>
+</body>
+
+</html> 
